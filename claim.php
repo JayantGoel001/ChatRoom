9def573bcecdbf486e93a711982d3d5949b27be0
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-07 21:39:20 +0530

    Initial Commit

--- claim.php
+++ claim.php
@@ -1,1 +1,9 @@
 <?php
+    $room = $_POST['room'];
+    if (strlen($room)>20 or strlen($room)<2){
+        $message = "Please Choose A Name between 2 to 20 characters.";
+        echo "<script language='JavaScript'>";
+        echo "alert('$message');";
+        echo "window.location='https://localhost/ChatRoom';";
+        echo "</script>";
+    }elseif ()
