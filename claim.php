d6b3146b453fd6f2744ab2b8b984433ecd8986d4
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-08 02:22:24 +0530

    Initial Commit

--- claim.php
+++ claim.php
@@ -1,9 +1,21 @@
 <?php
     $room = $_POST['room'];
+
     if (strlen($room)>20 or strlen($room)<2){
         $message = "Please Choose A Name between 2 to 20 characters.";
+
         echo "<script language='JavaScript'>";
         echo "alert('$message');";
-        echo "window.location='https://localhost/ChatRoom';";
+        echo "window.location='http://localhost:63342/ChatRoom/';";
         echo "</script>";
-    }elseif ()
+
+    }elseif (!ctype_alnum($room)){
+        $message = "Please Choose A Alpha Numeric Name";
+
+        echo "<script language='JavaScript'>";
+        echo "alert('$message');";
+        echo "window.location='http://localhost:63342/ChatRoom/';";
+        echo "</script>";
+    }else{
+        include 'db_connect.php';
+    }
