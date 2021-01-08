0a2b2a1c1aeee8b5b985f6e4aef572497b3d0c8d
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-08 21:48:47 +0530

    Initial Commit

--- rooms.php
+++ rooms.php
@@ -125,6 +125,7 @@
     </div>
 </div>
 
+<div class="product-device shadow-sm d-none d-md-block"></div>
 
 <label for="usermsg">
     <input type="text" id="usermsg" class="form-control" size="100%" name="usermsg" placeholder="Add Message">
@@ -132,7 +133,8 @@
 <br>
 <button class="btn btn-dark mt-3" id="submitsmg" name="submitsmg">SEND</button>
 
-<div class="product-device shadow-sm d-none d-md-block"></div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
