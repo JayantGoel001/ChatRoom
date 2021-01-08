a3b0f1b294369b8d1563d05977702ae4bf223d6d
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-08 22:25:10 +0530

    Initial Commit

--- rooms.php
+++ rooms.php
@@ -143,6 +143,15 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script type="text/javascript">
 
+    const input = document.getElementById("usermsg");
+
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("submitsmg").click();
+        }
+    });
+
     $("#submitsmg").click(function () {
         let clientmsg = $("#usermsg").val();
 
@@ -153,6 +162,7 @@
         },
         function (data,_) {
             document.getElementsByClassName('anyClass')[0].innerHeight=data;
+            $("#usermsg").val("");
             return false;
         });
     });
