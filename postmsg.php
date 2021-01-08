815339a3705731bc6a70fa2ba6a45938bc55c0ca
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-08 21:44:29 +0530

    Initial Commit

---
+++ postmsg.php
@@ -0,0 +1,19 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "chatroom";
+
+    $conn = mysqli_connect($servername,$username,$password,$database);
+
+    if (!$conn){
+        die("Failed To Connect" . mysqli_connect_error());
+    }
+
+    $msg = $_POST['text'];
+    $room = $_POST['room'];
+    $ip = $_POST['ip'];
+    $sql = "INSERT INTO `msgs` (`msg`,`room`,`ip`,`stime`) VALUES ('$msg','$room','$ip',CURRENT_TIMESTAMP )";
+    mysqli_query($conn,$sql);
+    mysqli_close($conn);
+
--- rooms.php
+++ rooms.php
@@ -130,12 +130,28 @@
     <input type="text" id="usermsg" class="form-control" size="100%" name="usermsg" placeholder="Add Message">
 </label>
 <br>
-<button class="btn btn-dark mt-3" name="submitsmg">SEND</button>
+<button class="btn btn-dark mt-3" id="submitsmg" name="submitsmg">SEND</button>
 
 <div class="product-device shadow-sm d-none d-md-block"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script type="text/javascript">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    let clientmsg = $("#usermsg").val();
 
+    $("#submitsmg").click(function () {
+        $.post("postmsg.php",{
+            text:clientmsg,
+            room:'<?php echo $room_name; ?>',
+            ip:'<?php echo $_SERVER['REMOTE_ADDR']; ?>'
+        },
+        function (data,status) {
+            document.getElementsByClassName('anyClass')[0].innerHeight=data;
+            return false;
+        });
+    });
+</script>
 </body>
 </html>
 
